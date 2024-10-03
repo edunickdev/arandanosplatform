1f54c10db602aeb6468c1c9861c88c5b507205bc
El Código Recuerda <114629663+edunickdev@users.noreply.github.com>
2024-10-03 15:50:42 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="grid grid-cols-12">
+      <div className="col-span-1"></div>
+      <div className="col-span-10"></div>
+      <div className="col-span-1"></div>
+    </div>
+  );
+}
+
+export default App;
